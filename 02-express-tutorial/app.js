aa29d8e835a1914d6a96714f76804f7435293152
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-09-24 13:29:57 +0900

    Completed express basic

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,27 @@
-console.log('Express Tutorial')
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Home Page");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("About");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>resource not found</h1>");
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000");
+});
+
+//http methods:
+// app.get = get/read data from server
+// app.post = place an order (send data)
+// app.put = update specific data (params + send data)
+// app.delete = delete specific data (path params)
+// app.all
+// app.use
+// app.listen
